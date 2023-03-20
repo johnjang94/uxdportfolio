4cb145c13b6cae42c934fa42fb50c2cf876602a8
John Jang <jscolbe9675@gmail.com>
2023-03-19 23:34:55 -0400

    third commit

--- coding/library/overview/overview.js
+++ coding/library/overview/overview.js
@@ -1,35 +1,24 @@
-// let originalGlassvu = document.getElementById("#overview1");
-
-// if (originalGlassvu.addEventListener("click")) {
-//   originalGlassvu.style.width = "800px";
-//   originalGlassvu.style.height = "500px";
-// }
-
-// CORRECTED BY ChatGPT
-
-// --> ChatGPT's idea
 function expandImage() {
   let img = document.getElementById("overview1");
+  let message = document.getElementById("message");
+  let name = document.getElementById("glassvu");
+  let box = document.getElementById("image-container");
+
   if (!img.classList.contains("expanded")) {
+    box.style.backgroundColor = "#FAFAFA";
+    box.style.width = "950px";
+    box.style.height = "650px";
+    box.style.marginLeft = "100px";
+    name.style.display = "none";
     img.classList.add("expanded");
+    img.style.marginTop = "20px";
+    img.style.marginLeft = "25px";
+    message.style.display = "block";
+    message.style.marginLeft = "25px";
+    message.style.marginBottom = "20px";
+  } else {
+    box.style.display = "none";
+    img.classList.remove("expanded");
+    message.style.display = "none";
   }
 }
-
-// --> My idea being corrected.
-// let originalGlassvu = document.getElementById("overview1");
-
-// originalGlassvu.addEventListener("click", function () {
-//   originalGlassvu.style.width = "800px";
-//   originalGlassvu.style.height = "500px";
-// });
-
-// Other possible ideas that I had for this part:
-
-// // originalGlassvu.addEventListener("click", expansion);
-
-// // function expansion() {
-// //   originalGlassvu.style.width = "800px";
-// //   originalGlassvu.style.height = "500px";
-// // }
-
-// // expansion();
