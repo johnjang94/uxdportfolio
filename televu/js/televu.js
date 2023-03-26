0538578191e6889d8d4bdc2e66e290697b5b8756
John Jang <jscolbe9675@gmail.com>
2023-03-26 13:27:00 -0400

    fourth commit

--- televu/js/televu.js
+++ televu/js/televu.js
@@ -1,126 +1,126 @@
-// =============== RESOURCE FROM THE LIBRARY =======================
-function expandImage() {
-  let img = document.getElementById("overview1");
-  let name = document.getElementById("glassvu");
-  let message = document.getElementById("message");
-  // Background for first picture
-  let box = document.getElementById("image-container");
-  let closeBtn = document.getElementById("close-btn");
-  // Second picture
-  let img2 = document.getElementById("overview2");
-  let name2 = document.getElementById("reportvu");
-  // Third picture
-  let img3 = document.getElementById("overview3");
-  let name3 = document.getElementById("oldvid");
+// DESCRIPTIVE TAGS FOR THE FIRST PICTURE (Original GlassVU)
+let image1 = document.getElementById("overview1");
+let name1 = document.getElementById("name1");
+// == ONCE EXPANDED, THE DETAIL TO SHOW UP FOR THE FIRST PICTURE (Original GlassVU) ==
+let message1 = document.getElementById("message1");
+let box1 = document.getElementById("image-container1");
+let closeBtn1 = document.getElementById("close-btn1");
 
-  // Hide overview3 completely (picture + name)
-  img3.style.display = "none";
-  name3.style.display = "none";
+// DESCRIPTIVE TAGS FOR THE SECOND PICTURE (Original ReportVU)
+let image2 = document.getElementById("overview2");
+let name2 = document.getElementById("name2");
+//  == ONCE EXPANDED, THE DETAIL TO SHOW UP FOR THE SECOND PICTURE (Original ReportVU) ==
+let message2 = document.getElementById("message2");
+let box2 = document.getElementById("image-container2");
+let closeBtn2 = document.getElementById("close-btn2");
 
-  // Hide overview2 completely (picture + name)
-  img2.style.display = "none";
+// DESCRIPTIVE TAGS FOR THE THIRD PICTURE (Original Video)
+let image3 = document.getElementById("overview3");
+let name3 = document.getElementById("name3");
+//  == ONCE EXPANDED, THE DETAIL TO SHOW UP FOR THE SECOND PICTURE (Original ReportVU) ==
+let message3 = document.getElementById("message3");
+let box3 = document.getElementById("image-container3");
+let closeBtn3 = document.getElementById("close-btn3");
+
+// =============== FUNCTIONS TO BE DONE =======================
+// =============== GlassVU PICTURE EXPANSION =======================
+function expandImage1() {
+  // FIRST PICTURE (Original GlassVU) = HIDE THE DETAIL, THE CLOSE (X) BUTTON, AND THE NAME TAG
+  message1.style.display = "none";
+  closeBtn1.style.display = "none";
+  name1.style.display = "none";
+
+  // SECOND PICTURE (Original ReportVU) = HIDE THE PICTURE AND THE TAG
+  image2.style.display = "none";
   name2.style.display = "none";
 
-  // Hide message before expanding the image
-  message.style.display = "none";
-  closeBtn.style.display = "none";
+  // THIRD PICTURE (Original Video) = HIDE THE PICTURE AND THE TAG
+  image3.style.display = "none";
+  name3.style.display = "none";
 
-  // Show everything else before expanding the image
-  // Background
-  box.style.backgroundColor = "#FAFAFA";
-  box.style.width = "970px";
-  box.style.height = "720px";
-  box.style.marginLeft = "120px";
-  box.style.transition = "ease-in-out";
-  box.style.transitionDuration = "1s";
-  // First picture
-  img.classList.add("expanded");
-  img.style.marginTop = "30px";
-  img.style.marginLeft = "30px";
-  img.style.width = "900px";
-  img.style.height = "550px";
-  img.style.transition = "ease-in-out";
-  img.style.transitionDuration = "1s";
-  name.style.display = "none";
+  // ONCE THE FIRST PICTURE IS "CLICKED" TO BE EXPANDED, THE BIGGER PICTURE AND THE DETAIL SHOWS UP WITH THE CLOSE (X) BUTTON
+  // BIGGER FIRST PICTURE
+  image1.classList.add("expanded");
+  image1.style.marginTop = "30px";
+  image1.style.marginLeft = "30px";
+  image1.style.width = "900px";
+  image1.style.height = "550px";
+  image1.style.transition = "ease-in-out";
+  image1.style.transitionDuration = "1s";
+  // THE FIRST PICTURE'S BACKGROUND
+  box1.style.backgroundColor = "#FAFAFA";
+  box1.style.width = "970px";
+  box1.style.height = "720px";
+  box1.style.marginLeft = "120px";
+  box1.style.transition = "ease-in-out";
+  box1.style.transitionDuration = "1s";
 
+  // THE DETAIL FOR THE FIRST PICTURE AND THE CLOSE (X) BUTTON WILL ONLY SHOW ONCE THE PICTURE HAS BEEN COMPLETELY EXPANDED
   setTimeout(function () {
-    message.style.display = "block";
-    message.style.marginLeft = "25px";
-    message.style.marginBottom = "20px";
+    message1.style.display = "block";
+    message1.style.marginLeft = "25px";
+    message1.style.marginBottom = "20px";
 
     // Show close button after expanding the image
-    closeBtn.style.display = "block";
-    closeBtn.style.marginTop = "-700px";
-    closeBtn.style.marginLeft = "610px";
+    closeBtn1.style.display = "block";
+    closeBtn1.style.marginTop = "-700px";
+    closeBtn1.style.marginLeft = "610px";
   }, 1000);
 }
 
-function collapseImage() {
-  let img = document.getElementById("overview1");
-  let name = document.getElementById("glassvu");
-  let message = document.getElementById("message");
+// =============== GlassVU PICTURE COLLAPSE =======================
+function collapseImage1() {
+  // HIDE THE FIRST PICTURE (GlassVU) DETAIL, ITS CLOSE (X) BUTTON, AND ITS TAG NAME
+  message1.style.display = "none";
+  closeBtn1.style.display = "none";
+  name1.style.display = "none";
 
-  // Second picture
-  let img2 = document.getElementById("overview2");
-  let name2 = document.getElementById("reportvu");
-
-  // Third picture
-  let img3 = document.getElementById("overview3");
-  let name3 = document.getElementById("oldvid");
-
-  // Background
-  let box = document.getElementById("image-container");
-  let closeBtn = document.getElementById("close-btn");
-
-  // hide overview2 and reportvu before collapsing the image
-  img2.style.display = "none";
+  // HIDE THE SECOND PICTURE AND ITS TAG NAME
+  image2.style.display = "none";
   name2.style.display = "none";
-  closeBtn.style.display = "none";
 
-  // hide glassvu before collapsing the image
-  name.style.display = "none";
+  // HIDE THE THIRD PICTURE AND ITS TAG NAME
+  image3.style.display = "none";
+  name3.style.display = "none";
 
-  // Collapse the image
-  img.classList.remove("expanded");
-  img.style.width = "328px";
-  img.style.height = "217px";
-  img.style.marginTop = "0px";
-  img.style.marginLeft = "0px";
-  img.style.transition = "ease-in-out";
-  img.style.transitionDuration = "1s";
+  // ONCE THE FIRST PICTURE IS "CLICKED" TO BE COLLAPSED, THE PICTURE SHOULD BE SMALL AND ITS TAG NAME SHOULD APPEAR
+  // SMALLER FIRST PICTURE
+  image1.classList.remove("expanded");
+  image1.style.width = "328px";
+  image1.style.height = "217px";
+  image1.style.marginTop = "0px";
+  image1.style.marginLeft = "0px";
+  image1.style.transition = "ease-in-out";
+  image1.style.transitionDuration = "1s";
+  // COLLAPSE THE BACKGROUND
+  box1.style.backgroundColor = "#FFFFFF";
+  box1.style.transition = "ease-in-out";
+  box1.style.transitionDuration = "1s";
 
-  // collapse the background
-  box.style.backgroundColor = "#FFFFFF";
-  box.style.transition = "ease-in-out";
-  box.style.transitionDuration = "1s";
-  message.style.display = "none";
-  name.style.marginLeft = "3px";
-
+  // AFTER THE COLLAPSING ANIMATION, THE PICTURES AND ITS TAGS SHOULD ALL SHOW UP
   setTimeout(function () {
-    // Show overview3 and original video after collapsing the image
-    img3.style.display = "inline";
-    img3.style.marginTop = "-720px";
-    name3.style.display = "inline";
-    name3.style.marginTop = "-490px";
-    // Show overview2 and reportvu after collapsing the image
-    img2.style.display = "inline";
-    img2.style.marginTop = "-720px";
+    // SHOW THE FIRST PICTURE AND ITS TAG NAME
+    image1.style.display = "inline";
+    name1.style.display = "inline";
+    name1.style.marginLeft = "119px";
+    // SHOW THE SECOND PICTURE AND ITS TAG NAME
+    image2.style.display = "inline";
+    image2.style.marginTop = "-720px";
     name2.style.display = "inline";
     name2.style.marginTop = "-490px";
-    // Show glassvu after collapsing the image
-    img.style.display = "inline";
-    name.style.display = "inline";
-    name.style.marginLeft = "119px";
+    // SHOW THE THIRD PICTURE AND ITS TAG NAME
+    image3.style.display = "inline";
+    image3.style.marginTop = "-720px";
+    name3.style.display = "inline";
+    name3.style.marginTop = "-490px";
   }, 1000);
 }
 
 function toggleImage() {
-  let img = document.getElementById("overview1");
-
-  if (!img.classList.contains("expanded")) {
-    expandImage();
+  if (!image1.classList.contains("expanded")) {
+    expandImage1();
   } else {
-    collapseImage();
+    collapseImage1();
   }
 }
 
