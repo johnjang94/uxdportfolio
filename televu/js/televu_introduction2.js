485ddd3b111682cbdb5ee68737179b675dd56482
John Jang <jscolbe9675@gmail.com>
2023-03-31 23:02:53 -0400

    sixteenth commit

--- televu/js/televu_introduction2.js
+++ televu/js/televu_introduction2.js
@@ -1,38 +1,23 @@
-// ======== MORE INFORMATION DETAIL IN POP-UP WINDOWS ==========
-// Get the button that opens the modal
-let btn = document.querySelectorAll("button.modal-button");
-
-// All page modals
-let modals = document.querySelectorAll(".modal");
-
-// Get the <span> element that closes the modal
-let spans = document.getElementsByClassName("close");
-
-// When the user clicks the button, open the modal
-for (let i = 0; i < btn.length; i++) {
-  btn[i].onclick = function (e) {
-    e.preventDefault();
-    modal = document.querySelector(e.target.getAttribute("href"));
-    modal.style.display = "block";
-  };
+function toggleBox() {
+  $("#box").slideToggle(500);
 }
 
-// When the user clicks on <span> (x), close the modal
-for (let i = 0; i < spans.length; i++) {
-  spans[i].onclick = function () {
-    for (let index in modals) {
-      if (typeof modals[index].style !== "undefined")
-        modals[index].style.display = "none";
-    }
-  };
+function toggleBox2() {
+  $("#box2").slideToggle(500);
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target.classList.contains("modal")) {
-    for (let index in modals) {
-      if (typeof modals[index].style !== "undefined")
-        modals[index].style.display = "none";
-    }
-  }
-};
+function toggleBox3() {
+  $("#box3").slideToggle(500);
+}
+
+function toggleBox4() {
+  $("#box4").slideToggle(500);
+}
+
+function toggleBox5() {
+  $("#box5").slideToggle(500);
+}
+
+function toggleBox6() {
+  $("#box6").slideToggle(500);
+}
