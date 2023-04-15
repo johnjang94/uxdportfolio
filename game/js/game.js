e92818db28c33b66a4e778042bfc5e89b78367b2
John Jang <jscolbe9675@gmail.com>
2023-04-15 15:50:49 -0400

    nineteenth commit

---
+++ game/js/game.js
@@ -0,0 +1,23 @@
+const slideshow = document.getElementById("slide-show");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+const slideInterval = setInterval(nextSlide, 3000);
+
+slideCount++;
+// Check if the slide count is equal to 5
+if (slideCount === 5) {
+  // Set a delay of 8 seconds before displaying the next slide
+  setTimeout(function () {
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].style.display = "block";
+  }),
+    10000;
+} else {
+  slides[currentSlide].style.display = "block";
+}
+
+function nextSlide() {
+  slides[currentSlide].style.display = "none";
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].style.display = "block";
+}
