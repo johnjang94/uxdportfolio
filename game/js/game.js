ca49c0de74c4b0904f5ac5db05674cce97948711
John Jang <jscolbe9675@gmail.com>
2023-04-16 17:05:58 -0400

    twentieth commit

--- game/js/game.js
+++ game/js/game.js
@@ -11,10 +11,7 @@ if (slideCount === 5) {
 }
 
 function nextSlide() {
-  if (slides !== slides[currentSlide]) {
-    slides.style.display = "none";
-  } else {
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].style.display = "block";
-  }
+  slides[currentSlide].style.display = "none";
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].style.display = "block";
 }
