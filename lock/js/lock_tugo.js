846b57d6e60c814535f5f3005c8646ff772b8cd0
John Jang <jscolbe9675@gmail.com>
2023-04-16 11:19:26 -0400

    twentieth commit

---
+++ lock/js/lock_tugo.js
@@ -0,0 +1,23 @@
+const lock = document.querySelector(".lock");
+const passwordInput = document.getElementById("password-input");
+
+let resultArea = document.getElementById("result-area");
+
+// Add event listener to the input box to listen for changes
+passwordInput.addEventListener("change", function () {
+  // Check if the entered password is correct
+  if (passwordInput.value === "Canada!004") {
+    // If the password is correct, add a CSS class to the lock element to unlock it
+    lock.classList.add("unlocked");
+    passwordInput.classList.add("correct-password");
+    (resultArea.textContent = "You may enter."),
+      (resultArea.style.color = "green");
+    window.location =
+      "https://johnjang94.github.io/uxdportfolio/tugo_introduction.html";
+  } else {
+    (resultArea.textContent =
+      "That is not a correct password. Please try again."),
+      (resultArea.style.color = "red");
+    passwordInput.style.borderColor = "red";
+  }
+});
